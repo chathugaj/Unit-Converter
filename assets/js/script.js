dae061b9e557930adcdbc9feb9985f16b474c161
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2023-07-03 02:57:52 +0200

    Refactoring and set default values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,52 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let meassures = document.getElementsByTagName("option");
-  /**for (let measure of meassures) {
-    measure.addEventListener("select", function () {
-      console.log(this);
-    });
-  }*/
   let measuretype1 = document.getElementById("measuretype");
+  let measrueTypeVal = measuretype1.value;
+  let pressureUnits2 = ["Torr", "pa", "bar"];
+  let conversionA = document.getElementById("conversion");
+  let conversionB = document.getElementById("conversion-1");
+  if (measrueTypeVal === "pressure") {
+    for (let unit of pressureUnits2) {
+      createUnitOption(unit, conversionA);
+      createUnitOption(unit, conversionB);
+    }
+  }
+
   measuretype1.addEventListener("change", function (event) {
     let conversionA = document.getElementById("conversion");
     let conversionB = document.getElementById("conversion-1");
     conversionA.innerHTML = "";
     conversionB.innerHTML = "";
 
+    /* loading mass unit**/
     let massUnits2 = ["kg", "g", "mg", "lbs", "Oz"];
     if (event.target.value === "mass") {
       for (let unit of massUnits2) {
-        let option = document.createElement("option");
-        conversionA.appendChild(option);
-        option.innerHTML = unit;
-        let option1 = document.createElement("option");
-        conversionB.appendChild(option1);
-        option1.innerHTML = unit;
+        createUnitOption(unit, conversionA);
+        createUnitOption(unit, conversionB);
       }
     }
-
+    /* loading temperature unit**/
     let tempUnits2 = ["Degree celcius", "fahrenheit", "kelvin"];
     if (event.target.value === "temprature") {
       for (let unit of tempUnits2) {
-        let option = document.createElement("option");
-        conversionA.appendChild(option);
-        option.innerHTML = unit;
-        let option1 = document.createElement("option");
-        conversionB.appendChild(option1);
-        option1.innerHTML = unit;
+        createUnitOption(unit, conversionA);
+        createUnitOption(unit, conversionB);
       }
     }
-
+    /* loading speed unit**/
     let speedUnits2 = ["km/h", "mp/h", "m/s"];
     if (event.target.value === "speed") {
       for (let unit of speedUnits2) {
-        let option = document.createElement("option");
-        conversionA.appendChild(option);
-        option.innerHTML = unit;
-        let option1 = document.createElement("option");
-        conversionB.appendChild(option1);
-        option1.innerHTML = unit;
+        createUnitOption(unit, conversionA);
+        createUnitOption(unit, conversionB);
       }
     }
+    /* loading pressure unit**/
 
-    let pressureUnits2 = ["Torr", "pa", "bar"];
     if (event.target.value === "pressure") {
       for (let unit of pressureUnits2) {
-        let option = document.createElement("option");
-        conversionA.appendChild(option);
-        option.innerHTML = unit;
-        let option1 = document.createElement("option");
-        conversionB.appendChild(option1);
-        option1.innerHTML = unit;
+        createUnitOption(unit, conversionA);
+        createUnitOption(unit, conversionB);
       }
     }
   });
@@ -65,3 +55,9 @@ document.addEventListener("DOMContentLoaded", function () {
 function runConverter() {}
 
 function calculateConversion() {}
+/* declare a function to reuse create , appendChild and populate measuring unit to option elements  **/
+function createUnitOption(unit, selectElement) {
+  let option = document.createElement("option");
+  selectElement.appendChild(option);
+  option.innerHTML = unit;
+}
