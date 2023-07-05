c988b47e8200e53acf541f25b3c44b50e8a28c0f
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2023-07-05 18:07:04 +0200

    Add funtions in javascript to convert kg's to other units

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let pressureUnits2 = ["Torr", "pa", "bar"];
   let conversionA = document.getElementById("conversion");
   let conversionB = document.getElementById("conversion-1");
+
   if (measrueTypeVal === "pressure") {
     for (let unit of pressureUnits2) {
       createUnitOption(unit, conversionA);
@@ -54,6 +55,38 @@ document.addEventListener("DOMContentLoaded", function () {
   let convertA = document.getElementsByClassName("convert-number")[0];
   convertA.addEventListener("click", function (event) {
     console.log(convertA);
+    /* convert kg to other units**/
+    if (conversionA.value === "kg") {
+      if (conversionB.value === "g") {
+        let input1 = document.getElementById("number-box");
+        let inputValue = input1.value;
+        console.log(parseFloat(inputValue) * 1000);
+        let input2 = document.getElementById("number-box1");
+        input2.value = parseFloat(inputValue) * 1000;
+      }
+      if (conversionB.value === "mg") {
+        let input1 = document.getElementById("number-box");
+        let inputValue = input1.value;
+        console.log(1000000);
+        let input2 = document.getElementById("number-box1");
+        input2.value = parseFloat(inputValue) * 1000000;
+      }
+
+      if (conversionB.value === "lbs") {
+        let input1 = document.getElementById("number-box");
+        let inputValue = input1.value;
+        console.log(2.205);
+        let input2 = document.getElementById("number-box1");
+        input2.value = parseFloat(inputValue) * 2.205;
+      }
+      if (conversionB.value === "Oz") {
+        let input1 = document.getElementById("number-box");
+        let inputValue = input1.value;
+        console.log(35.274);
+        let input2 = document.getElementById("number-box1");
+        input2.value = parseFloat(inputValue) * 35.274;
+      }
+    }
   });
 });
 
